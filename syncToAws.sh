4c852920ed285e093dd56c09e37a0c0106c72b2a
tanico-kazuyo <kazuyo.rikudo@gmail.com>
2020-11-14 08:34:09 +0900

    sync codes

---
+++ syncToAws.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+src=/Users/macico/Dropbox/kaggle/kuloko/kuloko_handler
+dst=/Users/macico/Dropbox/kaggle/kuloko/kuloko_aws/functions
+target_zipdir=/Users/macico/Dropbox/kaggle/kuloko/kuloko_aws/functions/kuloko_handler
+zipdir=/Users/macico/Dropbox/kaggle/kuloko/kuloko_aws/functions/kuloko_handler.zip
+
+rsync -av --delete $src $dst
+echo "DONE"
+zip -r $zipdir $target_zipdir  -x "*.DS_Store" "*__MACOSX*"
+rm -rf $target_zipdir 
+echo "DONE"
