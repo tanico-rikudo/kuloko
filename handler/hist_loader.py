e6358372d5d1e4db24749123d0feded303a06e74
tanico-kazuyo <kazuyo.rikudo@gmail.com>
2022-02-13 09:11:27 +0900

    Indent

--- handler/hist_loader.py
+++ handler/hist_loader.py
@@ -17,48 +17,53 @@ from joblib import Parallel, delayed
 from datetime import datetime as dt
 from datetime import timedelta, timezone
 
-sys.path.append(os.environ['COMMON_DIR'])
+sys.path.append(os.environ["COMMON_DIR"])
 from util.exceptions import *
 from util import daylib
 from util import utils
 from util.config import ConfigManager
 
-cm = ConfigManager(os.environ['KULOKO_INI'])
+cm = ConfigManager(os.environ["KULOKO_INI"])
 dl = daylib.daylib()
 
 
 class HistDataHandler:
-
     def __init__(self, logger, general_config_ini=None, general_config_mode="DEFAULT"):
         self._logger = logger
         self.load_config(general_config_ini, general_config_mode)
         self.set_config()
-        self._logger.info('[DONE]API Initialized')
+        self._logger.info("[DONE]API Initialized")
 
     def load_config(self, general_config_ini, general_config_mode):
         if general_config_ini is None:
-            self.general_config = cm.load_ini_config(path=None, config_name="general", mode=general_config_mode)
+            self.general_config = cm.load_ini_config(
+                path=None, config_name="general", mode=general_config_mode
+            )
         else:
             self.general_config = general_config_ini[general_config_mode]
-        self._logger.info(f'[DONE]Load General Config. Mode={general_config_mode}')
+        self._logger.info(f"[DONE]Load General Config. Mode={general_config_mode}")
 
     def set_config(self):
-        self.remote_end_point = os.path.join(self.general_config.get('ENDPOINT_URL'),
-                                             self.general_config.get('HISTDATA_URL'))
-        self.local_end_point = self.general_config.get('LOCAL_HISTDATA_PATH')
-        self.n_usable_core = self.general_config.getint('N_USABLE_CORE')
+        self.remote_end_point = os.path.join(
+            self.general_config.get("ENDPOINT_URL"),
+            self.general_config.get("HISTDATA_URL"),
+        )
+        self.local_end_point = self.general_config.get("LOCAL_HISTDATA_PATH")
+        self.n_usable_core = self.general_config.getint("N_USABLE_CORE")
         self._logger.info("[DONE] Get end points={0}".format(self.remote_end_point))
-        self._logger.info('[DONE]Set Config from loaded config')
+        self._logger.info("[DONE]Set Config from loaded config")
 
     def get_remote_target_url(self, sym, kind, int_date=None):
         int_date = dl.dt_to_intD(dl.currentTime(0)) if int_date is None else int_date
         str_date = dl.intD_to_strD(int_date)
-        target_url = os.path.join(self.remote_end_point,
-                                  kind,
-                                  sym,
-                                  str_date[:4],
-                                  str_date[4:6],
-                                  str_date + "_" + sym + ".csv.gz")
+        target_url = os.path.join(
+            self.remote_end_point,
+            kind,
+            sym,
+            str_date[:4],
+            str_date[4:6],
+            str_date + "_" + sym + ".csv.gz",
+        )
         # self._logger.info("[DONE]Get remote  taget url:{0}".format(target_url))
         return target_url
 
@@ -66,12 +71,14 @@ class HistDataHandler:
         # differ from remote  path
         int_date = dl.dt_to_intD(dl.currentTime(0)) if int_date is None else int_date
         str_date = dl.intD_to_strD(int_date)
-        target_url = os.path.join(self.local_end_point,
-                                  kind,
-                                  sym,
-                                  str_date[:4],
-                                  str_date[4:6],
-                                  str_date + "_" + sym + ".csv.gz")
+        target_url = os.path.join(
+            self.local_end_point,
+            kind,
+            sym,
+            str_date[:4],
+            str_date[4:6],
+            str_date + "_" + sym + ".csv.gz",
+        )
         # self._logger.info("[DONE]Get local  taget url:{0}".format(target_url))
         return target_url
 
@@ -81,28 +88,39 @@ class HistDataHandler:
         remote_path = self.get_remote_target_url(sym, kind, int_date)
         save_path = local_path if is_save else None
         self.item_validation(kind, mode)
-        if mode == 'local':
+        if mode == "local":
             if not os.path.isfile(local_path):
                 self._logger.error("Fail to get hist data. path{0}".format(local_path))
             df = self.read_local_hist(local_path)
             self._logger.info("[DONE] Get local hist data. Path={0}".format(local_path))
 
-        elif mode == 'remote':
+        elif mode == "remote":
             df = self.download_remote_hist(remote_path, save_path=save_path)
-            self._logger.info("[DONE] Get remote hist data. Path={0}".format(remote_path))
-        elif mode == 'auto':
+            self._logger.info(
+                "[DONE] Get remote hist data. Path={0}".format(remote_path)
+            )
+        elif mode == "auto":
             if not os.path.isfile(local_path):
-                self._logger.info("No local hist data. Try: remote.  path{0}".format(local_path))
+                self._logger.info(
+                    "No local hist data. Try: remote.  path{0}".format(local_path)
+                )
                 df = self.download_remote_hist(remote_path, save_path=save_path)
-                self._logger.info("[DONE] Get remote hist data. Path={0}".format(remote_path))
+                self._logger.info(
+                    "[DONE] Get remote hist data. Path={0}".format(remote_path)
+                )
             else:
                 df = self.read_local_hist(local_path)
-                self._logger.info("[DONE] Get local hist data. Path={0}".format(local_path))
+                self._logger.info(
+                    "[DONE] Get local hist data. Path={0}".format(local_path)
+                )
 
         if df is not None:
-            # localize and time stamp format     
-            df['timestamp'] = df['timestamp'].apply(
-                lambda x: dl.dt_to_strYMDHMSF(dl.str_utc_to_dt_offset(x, is_Z=False, is_T=False)))
+            # localize and time stamp format
+            df["timestamp"] = df["timestamp"].apply(
+                lambda x: dl.dt_to_strYMDHMSF(
+                    dl.str_utc_to_dt_offset(x, is_Z=False, is_T=False)
+                )
+            )
 
         return df
 
@@ -110,18 +128,24 @@ class HistDataHandler:
         # Fetch
         try:
             response = requests.get(url)
-            if response.headers['Content-Type'] != 'text/csv':
-                raise Exception("Response content type is invalid. Type={0}".format(response.headers['Content-Type']))
+            if response.headers["Content-Type"] != "text/csv":
+                raise Exception(
+                    "Response content type is invalid. Type={0}".format(
+                        response.headers["Content-Type"]
+                    )
+                )
             self._logger.info("[DONE] Get historical data. url={0}".format(url))
 
         except Exception as e:
-            self._logger.error("Fail to get historical data. url={0}:{1}".format(url, e))
+            self._logger.error(
+                "Fail to get historical data. url={0}:{1}".format(url, e)
+            )
             return None
 
         binary_data = response.content
         data = self.convert_gzip_binary_to_plain(binary_data)
 
-        # save or return 
+        # save or return
         if save_path is None:
             return data
 
@@ -130,37 +154,44 @@ class HistDataHandler:
             try:
                 self.save_hist(binary_data, save_path)
             except Exception as e:
-                self._logger.error("Fail to save historical data. Url={0}:{1}".format(save_path, e))
+                self._logger.error(
+                    "Fail to save historical data. Url={0}:{1}".format(save_path, e)
+                )
             finally:
                 return data
 
     def save_hist(self, data, save_path):
         if os.path.isfile(save_path):
-            self._logger.warning("Filepath={0} is already exist. "
-                                 "However, download and overwirte forcefully".format(save_path))
+            self._logger.warning(
+                "Filepath={0} is already exist. "
+                "However, download and overwirte forcefully".format(save_path)
+            )
         save_dir = os.path.dirname(save_path)
         if not os.path.isdir(save_dir):
             os.makedirs(save_dir)
             self._logger.info("[DONE] Make dir:{0}".format(save_dir))
-        with open(save_path, 'wb') as f:
+        with open(save_path, "wb") as f:
             f.write(data)
         self._logger.info("[DONE] Save historical data. path:{0}".format(save_path))
 
     def read_local_hist(self, read_local_path):
         df = None
         try:
-            df = pd.read_csv(read_local_path, compression='gzip')
+            df = pd.read_csv(read_local_path, compression="gzip")
             self._logger.info("[DONE] Readed data. Path={0}".format(read_local_path))
         except Exception as e:
-            self._logger.warning("[Fialure] Reading hist data. Path={0}:{1} ".format(read_local_path, e))
+            self._logger.warning(
+                "[Fialure] Reading hist data. Path={0}:{1} ".format(read_local_path, e)
+            )
         return df
 
     def convert_gzip_binary_to_plain(self, data):
         import io
         import gzip
         import csv
+
         f = io.BytesIO(data)
-        fh_r = pd.read_csv(f, compression='gzip', encoding='utf-8')
+        fh_r = pd.read_csv(f, compression="gzip", encoding="utf-8")
         return fh_r
         # with gzip.GzipFile(fileobj=f,mode='rb' ) as fh:
         #     # Passing a binary file to csv.reader works in PY2
@@ -169,10 +200,14 @@ class HistDataHandler:
         #     # for row in reader:
         #     #     print(row)
 
-    def bulk_load(self, sym, kind, since_int_date, until_int_date, is_save=True, mode='auto'):
+    def bulk_load(
+        self, sym, kind, since_int_date, until_int_date, is_save=True, mode="auto"
+    ):
         target_dates = dl.get_between_date(since_int_date, until_int_date)
         dfs = Parallel(n_jobs=self.n_usable_core)(
-            delayed(self.get_file_hist)(sym, kind, _target_date, is_save, mode) for _target_date in target_dates)
+            delayed(self.get_file_hist)(sym, kind, _target_date, is_save, mode)
+            for _target_date in target_dates
+        )
         dfs = [df for df in dfs if df is not None]
         if len(dfs) == 0:
             self._logger.warning("ALL object is failure.")
@@ -180,13 +215,17 @@ class HistDataHandler:
         return pd.concat(dfs, axis=0)
 
     def item_validation(self, kind, mode):
-        assert kind in ['trades', 'orderbooks'], "No such option. Mode={kind}"
-        assert mode in ['local', 'remote', 'auto'], "{kind} has no such option. Mode={mode}"
-        if kind == 'trades':
+        assert kind in ["trades", "orderbooks"], "No such option. Mode={kind}"
+        assert mode in [
+            "local",
+            "remote",
+            "auto",
+        ], "{kind} has no such option. Mode={mode}"
+        if kind == "trades":
             pass
-        elif kind == 'orderbooks':
-            if mode == 'auto':
-                mode == 'local'
+        elif kind == "orderbooks":
+            if mode == "auto":
+                mode == "local"
                 self._logger(f"{kind} fetch mode is changed to local mode.")
             else:
                 pass
